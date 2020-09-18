2742da7bd770408772dbaaa6fbcbd446ecc22f88
shelby_soutter <ssa.soutter@gmail.com>
2020-09-18 11:52:39 -0400

    search returns no image photo when poster is unavailable. search returns friendly message when no results available.

--- src/components/SearchMovies.vue
+++ src/components/SearchMovies.vue
@@ -2,10 +2,12 @@
     <div class="search">
         <h1>Search</h1>
         <input type="text" v-model="query" @keyup="getMovies(query)">
-        <div v-for="result in results" :key="result.id">
+        <div v-for="result in results" :key="result.id"> 
             <p>{{result.title}}</p>
-            <img v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" width="100px">
+            <img v-if="result.poster_path != null" v-bind:src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" width="100px">
+            <img v-if="result.poster_path == null && result.title != null" src='https://thumbs.dreamstime.com/b/no-image-available-icon-flat-vector-no-image-available-icon-flat-vector-illustration-132482953.jpg' width="100px">
         </div>
+        <p v-if="this.results.length == 0">Sorry! There are no movies available matching your search.</p>
     </div>
 </template>
 
@@ -21,52 +23,11 @@ export default {
             apiKey: "edbf0621b39a1db6f6ce24db4800ad5c"
         }
     },
-    methods: {
-        // async getURL() {
-        //     let URL = 'https://api.themoviedb.org/3/search/movie?api_key=edbf0621b39a1db6f6ce24db4800ad5c&query=superman'
-        //     let response = await fetch(URL)
-        //     .then(response => response.json())
-        // }
-        
-        
+    methods: { 
         getMovies(query) {
-            // const requestOptions = {
-            // method: 'GET',
-            // headers: { 
-            // 'Content-Type': 'application/json',
-            // 'Authorization': 'edbf0621b39a1db6f6ce24db4800ad5c'
-            // }};
-            // fetch('https://api.themoviedb.org/3/search/movie?', requestOptions)
-            //     .then(response => response.json())
-            //     .then(data => this.postId = data.id);
-            // }
             let URL = 'https://api.themoviedb.org/3/search/movie?api_key=edbf0621b39a1db6f6ce24db4800ad5c&query=' + query
             axios.get(URL).then(response => { this.results = response.data.results })
-            // this.responseAvailable = false
-            // fetch(URL, {
-            //     "method": "GET",
-            //     "headers": {
-            //         "Content-type": "application/json; charset=utf-8"
-            //     }
-            // }) 
-            // .then(response => {
-            //     if(response.ok) {
-            //         this.results = response
-            //         return response.json()
-
-            //     } else {
-            //         alert("Server returned " + response.status + " : " + response.statusText)
-            //     }
-            // })
-            // .then(response => {
-            //     this.results = response
-            //     this.responseAvailable = true
-            // })
-            // .catch(err => {
-            //     console.log(err)
-            // })
-        }
-            
+        }   
     }
 }
 </script>
